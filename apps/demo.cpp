f3ab5ebe029efa2c2d7ac7964b3ee62ac882028f
Adam Abed Abud <adam.abed.abud@cern.ch>
2021-10-29 08:53:48 +0200

    Trying to decode SSP frames

--- apps/demo.cpp
+++ apps/demo.cpp
@@ -13,9 +13,11 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #include "DAQDecoder.hpp" 
 #include "utils.hpp"
+
 
 
 int main(int argc, char** argv){
@@ -30,12 +32,33 @@ int main(int argc, char** argv){
     std::cout << "Number of events to read: " << num_trs << std::endl;
   }   
 
+
+  std::cout << "Before DAQDecoder \n";
+
   DAQDecoder decoder = DAQDecoder(argv[1], num_trs);
 
   std::vector<std::string> datasets_path = decoder.get_fragments(num_trs);
   //std::vector<std::string> datasets_path = decoder.get_trh(num_trs);
+ 
+  std::cout << "Start to read" << std::endl;
+ 
+  // Read all the fragments
+  std::ofstream output_file("./ssp_frames_total.txt");
   for (auto& element : datasets_path) {
-    read_fragment(decoder.get_frag_ptr(element));
+    auto vector_adc = ReadSSPFrag(decoder.get_frag_ptr(element));
+    std::cout <<" Read fragment " << std::endl; 
+    for (const auto &e : vector_adc) output_file << e << "\n";  
   }
+  std::cout << "Finished reading" << std::endl;
+  
+  // Read only one fragment
+  /*
+  auto frag = decoder.get_frag_ptr(datasets_path[0]);
+  std::ofstream output_file("./ssp_frames.txt");
+  if (frag->get_fragment_type() == dunedaq::dataformats::FragmentType::kPDSData) {
+    auto daphne_frames = ReadSSPFrag(std::move(frag));
+    for (const auto &e : daphne_frames) output_file << e << "\n";  
+  }
+  */
   return 0;
 }
--- apps/utils.hpp
+++ apps/utils.hpp
@@ -2,8 +2,7 @@
 #include <iostream>
 
 #include "dataformats/wib/WIBFrame.hpp"
-#include "dataformats/daphne/DAPHNEFrame.hpp"
-
+#include "dataformats/ssp/SSPTypes.hpp"
 
 void ReadWibFrag(std::unique_ptr<dunedaq::dataformats::Fragment> frag) {
   size_t raw_data_packets = (frag->get_size() - sizeof(dunedaq::dataformats::FragmentHeader)) / sizeof(dunedaq::dataformats::WIBFrame);
@@ -20,20 +19,67 @@ void ReadWibFrag(std::unique_ptr<dunedaq::dataformats::Fragment> frag) {
   return;
 }
 
-
-void ReadSSPFrag(std::unique_ptr<dunedaq::dataformats::Fragment> frag) {
+/*
+std::vector<int> ReadSSPFrag(std::unique_ptr<dunedaq::dataformats::Fragment> frag) {
     size_t raw_data_packets = (frag->get_size() - sizeof(dunedaq::dataformats::FragmentHeader)) / sizeof(dunedaq::dataformats::DAPHNEFrame);
     std::cout << "Fragment contains " << raw_data_packets << " DAPHNE frames" << std::endl;
+
+    // Create the vector to dump the DAPHNE frames: for each frame we have 320 channels
+    std::vector<int> ssp_frames; 
+    ssp_frames.reserve(320*raw_data_packets);
+
+    // Loop through all the packets
     for (size_t i=0; i < raw_data_packets; ++i) {
       auto dfptr = reinterpret_cast<dunedaq::dataformats::DAPHNEFrame*>(frag->get_data()+i*sizeof(dunedaq::dataformats::DAPHNEFrame));
-      if (i==0) {
-        std::cout << "Printout sampled timestamps in DAPHNE headers: " ;
-      }
-      if (i%1000 == 0) std::cout << "Timestamp " << i << ": " << dfptr->get_timestamp() << " ";
+      // Print timestamp of frame
+      std::cout << "Timestamp: " << dfptr->get_timestamp() << "  ";
+      std::cout << std::endl;
+
+      // Dump the DAPHNE frames inside the vector
+      for (int i=0; i<320; ++i) { ssp_frames.push_back(dfptr->get_t(i)); }
+      if (i == 0) std::cout << "Timestamp " << i << ": " << dfptr->get_timestamp() << " ";
     }
     std::cout << std::endl;
-  return;
+  return ssp_frames;
 }
+*/
+
+std::vector<int> ReadSSPFrag(std::unique_ptr<dunedaq::dataformats::Fragment> frag) {
+    std::vector<int> ssp_frames; 
+    auto ssp_event_header_ptr = reinterpret_cast<dunedaq::dataformats::EventHeader*>(frag->get_data()); 
+    std::cout << ssp_event_header_ptr->group2 << std::endl;
+    
+    unsigned long ts = 0;
+    for (unsigned int iword = 0 ; iword <= 3; ++iword) {
+      ts += ((unsigned long)(ssp_event_header_ptr->timestamp[iword])) << 16 * iword;
+    }
+    std::cout << " Timestamp: " << ts << std::endl;
+    size_t raw_data_packets = (frag->get_size() - sizeof(dunedaq::dataformats::FragmentHeader) );
+    std::cout << "Raw data packets: " << raw_data_packets << std::endl; 
+    std::cout << "SSP event length: "  << ssp_event_header_ptr->length << std::endl; 
+    std::cout << "Size of event channels: " <<  ssp_event_header_ptr->length - sizeof(*ssp_event_header_ptr) << std::endl ;
+    std::cout << "Raw data packates without event header: " <<  raw_data_packets - sizeof(*ssp_event_header_ptr) << std::endl ;
+    std::cout << "Number of SSP events: " <<  raw_data_packets /ssp_event_header_ptr->length  << std::endl ;
+
+    unsigned int nADC=((ssp_event_header_ptr->length-sizeof(dunedaq::dataformats::EventHeader))/sizeof(unsigned int))*2;
+    std::cout << "Number of ADCs: " << nADC << std::endl;
+    
+    unsigned short* adcPointer=reinterpret_cast<unsigned short*>(frag->get_data());
+    adcPointer += sizeof(dunedaq::dataformats::EventHeader)/sizeof(unsigned short);
+    unsigned short* adc; 
+    for (size_t idata=0; idata< nADC; ++idata) {
+      adc = adcPointer + idata;
+      //std::cout << "Value of ADC: " << *adc << std::endl;
+      ssp_frames.push_back(*adc);  
+    }
+    
+
+    
+    return ssp_frames;
+}
+
+
+
 
 
 void read_fragment(std::unique_ptr<dunedaq::dataformats::Fragment> frag){
