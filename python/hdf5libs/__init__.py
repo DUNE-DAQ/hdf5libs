aab66f40b68222a73268a5e0f0e516bbf576d132
Alessandro Thea <alessandro.thea@cern.ch>
2022-10-24 11:19:24 +0200

    Fixing HDF5RawDataFile import in python

--- python/hdf5libs/__init__.py
+++ python/hdf5libs/__init__.py
@@ -1,4 +1,8 @@
 from ._daq_hdf5libs_py import *
+from ._daq_hdf5libs_py import _HDF5RawDataFile
+
+# from . import _daq_hdf5libs_py as x
+# print(dir(x))
 
 class HDF5RawDataFile(_HDF5RawDataFile):
 
