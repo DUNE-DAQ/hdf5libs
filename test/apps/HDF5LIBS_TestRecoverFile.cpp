61e447a4c4319ee25de28e6e2c925000da7c2808
np04daq <np04daq@np04-srv-024.cern.ch>
2024-11-18 23:32:17 +0100

    [KAB] Filled in the functionality in HDF5LIBS_TestRecoverFile to actually write the Attributes that might be needed to recover a file.

--- test/apps/HDF5LIBS_TestRecoverFile.cpp
+++ test/apps/HDF5LIBS_TestRecoverFile.cpp
@@ -66,24 +66,24 @@ main(int argc, char** argv)
 
   const std::string ifile_name = std::string(argv[1]);
 
-  // open the file for reading and writing
-  HDF5RawDataFile h5_raw_data_file(ifile_name, true);
+  // open the file for reading, initially
+  std::unique_ptr< HDF5RawDataFile> h5file_ptr(new HDF5RawDataFile(ifile_name, false));
 
   size_t last_modified_time = 0;
   struct stat stat_results;
   auto retcode = stat(ifile_name.c_str(), &stat_results);
-  if (retcode == 0) {last_modified_time = stat_results.st_mtime;}
+  if (retcode == 0) {last_modified_time = 1000 * stat_results.st_mtime;} // msec
 
-  std::string closing_timestamp = h5_raw_data_file.get_attribute("closing_timestamp", std::string(""));
+  std::string closing_timestamp = h5file_ptr->get_attribute("closing_timestamp", std::string(""));
 
-  size_t recorded_size = h5_raw_data_file.get_attribute("recorded_size", SIZE_MAX);
+  size_t recorded_size = h5file_ptr->get_attribute("recorded_size", SIZE_MAX);
 
   size_t calculated_recorded_size = 0;
-  auto records = h5_raw_data_file.get_all_record_ids();
+  auto records = h5file_ptr->get_all_record_ids();
   for (auto const& record_id : records) {
-    if (h5_raw_data_file.is_timeslice_type()) {
+    if (h5file_ptr->is_timeslice_type()) {
       try {
-	auto tsh_ptr = h5_raw_data_file.get_tsh_ptr(record_id);
+	auto tsh_ptr = h5file_ptr->get_tsh_ptr(record_id);
 	calculated_recorded_size += sizeof(*tsh_ptr);
       } catch (std::exception const& excpt) {
 	// bad record header, we'll skip the whole TimeSlice
@@ -91,17 +91,17 @@ main(int argc, char** argv)
       }
     } else {
       try {
-	auto trh_ptr = h5_raw_data_file.get_trh_ptr(record_id);
+	auto trh_ptr = h5file_ptr->get_trh_ptr(record_id);
 	calculated_recorded_size += trh_ptr->get_total_size_bytes();
       } catch (std::exception const& excpt) {
 	// bad record header, we'll skip the whole TriggerRecord
 	continue;
       }
     }
-    std::set<SourceID> frag_sid_list = h5_raw_data_file.get_fragment_source_ids(record_id);
+    std::set<SourceID> frag_sid_list = h5file_ptr->get_fragment_source_ids(record_id);
     for (auto const& source_id : frag_sid_list) {
       try {
-	auto frag_ptr = h5_raw_data_file.get_frag_ptr(record_id, source_id);
+	auto frag_ptr = h5file_ptr->get_frag_ptr(record_id, source_id);
 	calculated_recorded_size += frag_ptr->get_size();
       } catch (std::exception const& excpt) {
 	// nothing to do, just leave this fragment out of the sum
@@ -114,26 +114,50 @@ main(int argc, char** argv)
   std::cout << std::endl;
   std::cout << "========================================" << std::endl;
 
-  if (closing_timestamp == "") {
-    std::cout << "The \"closing_timestamp\" Attribute is *not* currently set in the file, and it will be " << std::endl;
-    std::cout << "    set to " << last_modified_time << " if/when the file is recovered." << std::endl;
-  } else {
-    std::cout << "The \"closing_timestamp\" Attribute in the file is currently set to " << closing_timestamp << "." << std::endl;
-  }
+  if (do_recovery) {
 
-  if (recorded_size == SIZE_MAX) {
-    std::cout << "The \"recorded_size\" Attribute is *not* currently set in the file, and it will be " << std::endl;
-    std::cout << "    set to " << calculated_recorded_size << " if/when the file is recovered." << std::endl;
-  } else {
-    std::cout << "The \"recorded_size\" Attribute in the file is currently set to " << recorded_size << "." << std::endl;
-  }
+    // re-open the file for reading and writing
+    h5file_ptr.reset();
+    h5file_ptr.reset(new HDF5RawDataFile(ifile_name, true));
 
-  if (ifile_name.rfind(HDF5RawDataFile::s_inprogress_suffix) != std::string::npos) {
-    std::cout << "The file *does* have the \"" << HDF5RawDataFile::s_inprogress_suffix << "\" suffix, "
-	      << "so it will be renamed if/when it is recovered." << std::endl;
+    std::cout << "Setting the \"recorded_size\" Attribute value to " << calculated_recorded_size << "." << std::endl;
+    h5file_ptr->write_attribute("recorded_size", calculated_recorded_size);
+
+    std::string file_closing_timestamp = std::to_string(last_modified_time);
+    std::cout << "Setting the \"closing_timestamp\" Attribute value to " << file_closing_timestamp << "." << std::endl;
+    h5file_ptr->write_attribute("closing_timestamp", file_closing_timestamp);
+
+    int64_t timestamp =
+      std::chrono::duration_cast<std::chrono::milliseconds>(system_clock::now().time_since_epoch()).count();
+    std::cout << "Setting the \"file_recovery_timestamp\" Attribute value to " << timestamp << "." << std::endl;
+    h5file_ptr->write_attribute("file_recovery_timestamp", timestamp);
+
+    // the HDF5RawDataFile Destructor will handle the file renaming, if that is needed.
+
   } else {
-    std::cout << "The file name does not have the \"" << HDF5RawDataFile::s_inprogress_suffix << "\" suffix, "
-	      << "so it will not be renamed." << std::endl;
+
+    if (closing_timestamp == "") {
+      std::cout << "The \"closing_timestamp\" Attribute is *not* currently set in the file, and it will be " << std::endl;
+      std::cout << "    set to " << last_modified_time << " if/when the file is recovered." << std::endl;
+    } else {
+      std::cout << "The \"closing_timestamp\" Attribute in the file is currently set to " << closing_timestamp << "." << std::endl;
+    }
+
+    if (recorded_size == SIZE_MAX) {
+      std::cout << "The \"recorded_size\" Attribute is *not* currently set in the file, and it will be " << std::endl;
+      std::cout << "    set to " << calculated_recorded_size << " if/when the file is recovered." << std::endl;
+    } else {
+      std::cout << "The \"recorded_size\" Attribute in the file is currently set to " << recorded_size << "." << std::endl;
+    }
+
+    if (ifile_name.rfind(HDF5RawDataFile::s_inprogress_suffix) != std::string::npos) {
+      std::cout << "The file *does* have the \"" << HDF5RawDataFile::s_inprogress_suffix << "\" suffix, "
+		<< "so it will be renamed if/when it is recovered." << std::endl;
+    } else {
+      std::cout << "The file name does not have the \"" << HDF5RawDataFile::s_inprogress_suffix << "\" suffix, "
+		<< "so it will not be renamed." << std::endl;
+    }
+
   }
 
   return 0;
