d83798de6c55ba1533a309f0a98c0ffbaec29e47
Andrew Mogan <amogan@fnal.gov>
2024-12-19 13:06:16 -0600

    Implement FileWriteFixture to reduce code duplication

--- unittest/HDF5WriteReadTimeSlice_test.cxx
+++ unittest/HDF5WriteReadTimeSlice_test.cxx
@@ -37,8 +37,6 @@ constexpr size_t element_count_pds = 4;
 
 const size_t components_per_record = element_count_tpc + element_count_pds;
 
-unsigned compression_level = 0;
-
 HDF5FileLayoutParameters
 create_file_layout_params()
 {
@@ -146,12 +144,108 @@ create_timeslice(int ts_num)
   return ts;
 }
 
-BOOST_AUTO_TEST_SUITE(HDF5WriteReadTimeSlice_test)
+struct FileWriteFixture 
+{
+  FileWriteFixture(int num_slices = 5, unsigned comp_lvl = 0) 
+    : timeslice_count(num_slices), 
+      compression_level(comp_lvl),
+      file_path(std::filesystem::temp_directory_path()),
+      hdf5_filename(
+        "demo" + std::to_string(getpid()) + "_" 
+        + std::string(getenv("USER")) + "_comp" 
+        + std::to_string(compression_level) + ".hdf5"),
+      fl_pars(create_file_layout_params()),
+      recorded_size_at_write(0)
+  {
+    delete_files_matching_pattern(file_path, hdf5_filename);
 
-BOOST_AUTO_TEST_CASE(WriteFileAndAttributes)
+    // convert file_params to json, allows for easy comp later
+    auto fl_pars = create_file_layout_params();
+
+    // create src-geo id map
+    auto srcid_geoid_map = create_srcid_geoid_map();
+    // create the file
+    std::unique_ptr<HDF5RawDataFile> h5file_ptr(new HDF5RawDataFile(file_path + "/" + hdf5_filename,
+                                                                    run_number,
+                                                                    file_index,
+                                                                    application_name,
+                                                                    fl_pars,
+                                                                    srcid_geoid_map,
+                                                                    compression_level));
+
+    BOOST_TEST_MESSAGE("Compression level in constructor: " << compression_level);
+
+    // write several events, each with several fragments
+    for (int timeslice_number = 1; timeslice_number <= timeslice_count; ++timeslice_number)
+      h5file_ptr->write(create_timeslice(timeslice_number));
+
+    // get recorded size for checking
+    recorded_size_at_write = h5file_ptr->get_recorded_size();
+    BOOST_TEST_MESSAGE("Recorded size at write: " << recorded_size_at_write);
+
+    h5file_ptr.reset(); // explicit destruction
+
+  }
+  ~FileWriteFixture() 
+  {
+    delete_files_matching_pattern(file_path, hdf5_filename);
+  }
+
+  //void read_file_attributes(std::string file_path, std::string hdf5_filename, HDF5FileLayoutParameters fl_pars)
+  void read_file_attributes()
+  {
+    // open file for reading now
+    //std::unique_ptr<HDF5RawDataFile> h5file_ptr = std::make_unique<HDF5RawDataFile>(file_path + "/" + hdf5_filename);
+    h5file_ptr.reset(new HDF5RawDataFile(file_path + "/" + hdf5_filename));
+
+    BOOST_TEST_MESSAGE("Compression level in read function: " << compression_level);
+
+    // check attributes
+    auto recorded_size_attr = h5file_ptr->get_attribute<size_t>("recorded_size");
+    BOOST_TEST_MESSAGE("recorded_size_attr: " << recorded_size_attr);
+    auto run_number_attr = h5file_ptr->get_attribute<size_t>("run_number");
+    auto file_index_attr = h5file_ptr->get_attribute<size_t>("file_index");
+    auto app_name_attr = h5file_ptr->get_attribute<std::string>("application_name");
+    auto record_type_attr = h5file_ptr->get_attribute<std::string>("record_type");
+    BOOST_REQUIRE_EQUAL(recorded_size_at_write, recorded_size_attr);
+    BOOST_REQUIRE_EQUAL(run_number, run_number_attr);
+    BOOST_REQUIRE_EQUAL(file_index, file_index_attr);
+    BOOST_REQUIRE_EQUAL(application_name, app_name_attr);
+    BOOST_REQUIRE_EQUAL("TimeSlice", record_type_attr);
+
+    // extract and check file layout parameters
+    auto file_layout_parameters_read = h5file_ptr->get_file_layout().get_file_layout_params();
+    BOOST_REQUIRE_EQUAL(fl_pars.to_json(), file_layout_parameters_read.to_json());
+  }
+
+  int timeslice_count;
+  unsigned compression_level;
+  std::string file_path;
+  std::string hdf5_filename;
+  HDF5FileLayoutParameters fl_pars;
+  size_t recorded_size_at_write;
+  std::unique_ptr<HDF5RawDataFile> h5file_ptr;
+};
+
+BOOST_FIXTURE_TEST_SUITE(HDF5WriteReadTimeSlice_test, FileWriteFixture)
+
+BOOST_AUTO_TEST_CASE(ReadFileAttributes) 
+{
+  FileWriteFixture fixture(5, 0);
+  read_file_attributes();
+}
+
+BOOST_AUTO_TEST_CASE(ReadCompressedFileAttributes) 
+{
+  FileWriteFixture fixture(10, 1);
+  read_file_attributes();
+}
+
+void read_file_datasets(unsigned compression_level)
 {
   std::string file_path(std::filesystem::temp_directory_path());
-  std::string hdf5_filename = "demo" + std::to_string(getpid()) + "_" + std::string(getenv("USER")) + ".hdf5";
+  std::string hdf5_filename = "demo" + std::to_string(getpid()) + "_" + std::string(getenv("USER")) + 
+                              "_comp" + std::to_string(compression_level) + ".hdf5";
   const int timeslice_count = 5;
 
   // delete any pre-existing files so that we start with a clean slate
@@ -163,40 +257,103 @@ BOOST_AUTO_TEST_CASE(WriteFileAndAttributes)
   // create src-geo id map
   auto srcid_geoid_map = create_srcid_geoid_map();
   // create the file
-  std::unique_ptr<HDF5RawDataFile> h5file_ptr(new HDF5RawDataFile(
-    file_path + "/" + hdf5_filename, run_number, file_index, application_name, fl_pars, srcid_geoid_map, compression_level));
+  std::unique_ptr<HDF5RawDataFile> h5file_ptr(new HDF5RawDataFile(file_path + "/" + hdf5_filename,
+                                                                  run_number,
+                                                                  file_index,
+                                                                  application_name,
+                                                                  fl_pars,
+                                                                  srcid_geoid_map,
+                                                                  compression_level));
 
   // write several events, each with several fragments
   for (int timeslice_number = 1; timeslice_number <= timeslice_count; ++timeslice_number)
     h5file_ptr->write(create_timeslice(timeslice_number));
-
-  // get recorded size for checking
-  size_t recorded_size_at_write = h5file_ptr->get_recorded_size();
 
   h5file_ptr.reset(); // explicit destruction
 
   // open file for reading now
   h5file_ptr.reset(new HDF5RawDataFile(file_path + "/" + hdf5_filename));
 
-  // check attributes
-  auto recorded_size_attr = h5file_ptr->get_attribute<size_t>("recorded_size");
-  auto run_number_attr = h5file_ptr->get_attribute<size_t>("run_number");
-  auto file_index_attr = h5file_ptr->get_attribute<size_t>("file_index");
-  auto app_name_attr = h5file_ptr->get_attribute<std::string>("application_name");
-  auto record_type_attr = h5file_ptr->get_attribute<std::string>("record_type");
-  BOOST_REQUIRE_EQUAL(recorded_size_at_write, recorded_size_attr);
-  BOOST_REQUIRE_EQUAL(run_number, run_number_attr);
-  BOOST_REQUIRE_EQUAL(file_index, file_index_attr);
-  BOOST_REQUIRE_EQUAL(application_name, app_name_attr);
-  BOOST_REQUIRE_EQUAL("TimeSlice", record_type_attr);
+  auto timeslices = h5file_ptr->get_all_timeslice_numbers();
+  BOOST_REQUIRE_EQUAL(timeslice_count, timeslices.size());
 
-  // extract and check file layout parameters
-  auto file_layout_parameters_read = h5file_ptr->get_file_layout().get_file_layout_params();
-  BOOST_REQUIRE_EQUAL(fl_pars.to_json(), file_layout_parameters_read.to_json());
+  auto first_timeslice = *(timeslices.begin());
+  auto last_timeslice = *(std::next(timeslices.begin(), timeslices.size() - 1));
+  BOOST_REQUIRE_EQUAL(1, first_timeslice);
+  BOOST_REQUIRE_EQUAL(timeslice_count, last_timeslice);
+
+  auto all_datasets = h5file_ptr->get_dataset_paths();
+  BOOST_REQUIRE_EQUAL(timeslice_count * (1 + components_per_record), all_datasets.size());
+
+  auto all_tsh_paths = h5file_ptr->get_timeslice_header_dataset_paths();
+  BOOST_REQUIRE_EQUAL(timeslice_count, all_tsh_paths.size());
+
+  auto all_frag_paths = h5file_ptr->get_all_fragment_dataset_paths();
+  BOOST_REQUIRE_EQUAL(timeslice_count * components_per_record, all_frag_paths.size());
+
+  // test access by name
+  std::unique_ptr<dunedaq::daqdataformats::TimeSliceHeader> trs_ptr;
+  trs_ptr = h5file_ptr->get_tsh_ptr(all_tsh_paths.at(2));
+  BOOST_REQUIRE_EQUAL(trs_ptr->timeslice_number, 3);
+  BOOST_REQUIRE_EQUAL(trs_ptr->run_number, run_number);
+
+  // test access by trigger number
+  trs_ptr = h5file_ptr->get_tsh_ptr(2);
+  BOOST_REQUIRE_EQUAL(trs_ptr->timeslice_number, 2);
+  BOOST_REQUIRE_EQUAL(trs_ptr->run_number, run_number);
+
+  std::unique_ptr<dunedaq::daqdataformats::Fragment> frag_ptr;
+
+  // test access by name
+  frag_ptr = h5file_ptr->get_frag_ptr(all_frag_paths.back());
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_trigger_number(), last_timeslice);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_run_number(), run_number);
+
+  // test access by trigger number, type, element
+  frag_ptr = h5file_ptr->get_frag_ptr(2, 0, "Detector_Readout", 0);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_trigger_number(), 2);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_run_number(), run_number);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_element_id().subsystem,
+                      dunedaq::daqdataformats::SourceID::Subsystem::kDetectorReadout);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_element_id().id, 0);
+
+  // test access by trigger number, type, element
+  frag_ptr = h5file_ptr->get_frag_ptr(4, 0, "Detector_Readout", 4);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_trigger_number(), 4);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_run_number(), run_number);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_element_id().subsystem,
+                      dunedaq::daqdataformats::SourceID::Subsystem::kDetectorReadout);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_element_id().id, 4);
+
+  // test access by passing in SourceID
+  dunedaq::daqdataformats::SourceID gid = { dunedaq::daqdataformats::SourceID::Subsystem::kDetectorReadout, 1 };
+  frag_ptr = h5file_ptr->get_frag_ptr(5, 0, gid);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_trigger_number(), 5);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_run_number(), run_number);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_element_id().subsystem,
+                      dunedaq::daqdataformats::SourceID::Subsystem::kDetectorReadout);
+  BOOST_REQUIRE_EQUAL(frag_ptr->get_element_id().id, 1);
 
   // clean up the files that were created
   delete_files_matching_pattern(file_path, hdf5_filename);
 }
+
+
+//BOOST_AUTO_TEST_SUITE(HDF5WriteReadTimeSlice_test)
+
+/*
+BOOST_AUTO_TEST_CASE(WriteFileAndAttributes)
+{
+  FileWriteFixture fixture(5, 0);
+  //read_file_attributes();
+}
+
+BOOST_AUTO_TEST_CASE(WriteCompressedFileAndAttributes)
+{
+  FileWriteFixture fixture(5, 1);
+  //read_file_attributes();
+}
+*/
 
 BOOST_AUTO_TEST_CASE(ReadFileDatasets)
 {
@@ -209,7 +366,6 @@ BOOST_AUTO_TEST_CASE(ReadFileDatasets)
 
   // convert file_params to json, allows for easy comp later
   auto fl_pars = create_file_layout_params();
-  fl_pars.digits_for_sequence_number = 4;
 
   // create src-geo id map
   auto srcid_geoid_map = create_srcid_geoid_map();
@@ -306,7 +462,7 @@ BOOST_AUTO_TEST_CASE(ReadFileMaxSequence)
 
   // convert file_params to json, allows for easy comp later
   auto fl_pars = create_file_layout_params();
-  fl_pars.digits_for_sequence_number = 4;
+  //fl_pars.digits_for_sequence_number = 4;
 
   // create src-geo id map
   auto srcid_geoid_map = create_srcid_geoid_map();
