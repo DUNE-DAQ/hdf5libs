5a059b100fe038217dfedd18d3c05f1d24a1db3f
Andrew Mogan <amogan@fnal.gov>
2026-03-20 12:09:59 -0500

    Add cout statement to make linter complain

--- src/HDF5RawDataFile.cpp
+++ src/HDF5RawDataFile.cpp
@@ -70,6 +70,8 @@ HDF5RawDataFile::HDF5RawDataFile(std::string file_name,
   write_attribute("creation_timestamp", file_creation_timestamp);
   write_attribute("application_name", application_name);
 
+  std::cout << "Here's a garbage cout statement." << std::endl;
+
   // set the file layout contents
   m_file_layout_ptr.reset(new HDF5FileLayout(fl_params));
   write_file_layout();
